2aba28f3e6bcbdebe88726807f4cc2cd41447e2f
Andrew Strange <andrew@gumboot.co.uk>
2023-12-10 14:44:06 +0000

    Day 10: Pipe Maze 2
    Failed solution, passes test, doesn't account for pockets.

--- solutions/d10pipemaze/pipemaze.go
+++ solutions/d10pipemaze/pipemaze.go
@@ -2,6 +2,7 @@ package d10pipemaze
 
 import (
 	"advent/solutions/utils"
+	"fmt"
 	"strconv"
 )
 
@@ -23,11 +24,13 @@ func buildGrid(xMax, yMax int) [][]bool {
 }
 
 func findFirstMove(pipes []string, x, y int) (int, int, Direction) {
-	c := pipes[y-1][x]
-	if c == '|' || c == '7' || c == 'F' {
-		return x, y - 1, North
+	if y != 0 {
+		c := pipes[y-1][x]
+		if c == '|' || c == '7' || c == 'F' {
+			return x, y - 1, North
+		}
 	}
-	c = pipes[y][x+1]
+	c := pipes[y][x+1]
 	if c == '-' || c == '7' || c == 'J' {
 		return x + 1, y, East
 	}
@@ -38,16 +41,32 @@ func findFirstMove(pipes []string, x, y int) (int, int, Direction) {
 	return x - 1, y, West
 }
 
-func TraversePipe(lines []string, startX, startY int) int {
-	nextX, nextY, direction := findFirstMove(lines, startX, startY)
+func TraversePipe(lines []string, startX, startY int) (int, int) {
+	xMax, yMax := len(lines[0]), len(lines)
+	visited, left, right := buildGrid(xMax, yMax), buildGrid(xMax, yMax), buildGrid(xMax, yMax)
+	nextX, nextY, firstDirection := findFirstMove(lines, startX, startY)
+	inDirection := firstDirection
+	visited[nextY][nextX] = true
+	left[nextY][nextX], right[nextY][nextX] = false, false
 	nextC := lines[nextY][nextX]
 	distance := 1
 	for nextC != 'S' {
-		nextX, nextY, direction = nextMove(nextC, nextX, nextY, direction)
+		nX, nY, outDirection := nextMove(nextC, nextX, nextY, inDirection)
+		handleOthers(nextX, nextY, xMax, yMax, inDirection, outDirection, visited, left, right)
+		nextX, nextY = nX, nY
+		inDirection = outDirection
 		nextC = lines[nextY][nextX]
+		visited[nextY][nextX] = true
+		left[nextY][nextX], right[nextY][nextX] = false, false
 		distance += 1
 	}
-	return distance / 2
+	handleOthers(startX, startY, xMax, yMax, inDirection, firstDirection, visited, left, right)
+	l, r := countArea(left), countArea(right)
+	fmt.Println(l, r)
+	if l < r {
+		return distance / 2, l
+	}
+	return distance / 2, r
 }
 
 func nextMove(c byte, x, y int, d Direction) (int, int, Direction) {
@@ -88,8 +107,82 @@ func nextMove(c byte, x, y int, d Direction) (int, int, Direction) {
 	}
 }
 
+func handleOthers(x, y, xMax, yMax int, in, out Direction, visited, left, right [][]bool) {
+	leftN, rightN := selectUnvisited(x, y, in, out)
+	for _, xy := range leftN {
+		if xy[0] >= 0 && xy[1] >= 0 && xy[0] < xMax && xy[1] < yMax {
+			if !visited[xy[1]][xy[0]] {
+				left[xy[1]][xy[0]] = true
+			}
+		}
+	}
+	for _, xy := range rightN {
+		if xy[0] >= 0 && xy[1] >= 0 && xy[0] < xMax && xy[1] < yMax {
+			if !visited[xy[1]][xy[0]] {
+				right[xy[1]][xy[0]] = true
+			}
+		}
+	}
+}
+
+func selectUnvisited(x, y int, in, out Direction) ([][]int, [][]int) {
+	if in == South {
+		switch out {
+		case East:
+			return nil, [][]int{{x - 1, y}, {x, y + 1}}
+		case South:
+			return [][]int{{x + 1, y}}, [][]int{{x - 1, y}}
+		case West:
+			return [][]int{{x + 1, y}, {x, y + 1}}, nil
+		}
+	}
+	if in == West {
+		switch out {
+		case South:
+			return nil, [][]int{{x - 1, y}, {x, y - 1}}
+		case West:
+			return [][]int{{x, y + 1}}, [][]int{{x, y - 1}}
+		case North:
+			return [][]int{{x - 1, y}, {x, y + 1}}, nil
+		}
+	}
+	if in == North {
+		switch out {
+		case West:
+			return nil, [][]int{{x + 1, y}, {x, y - 1}}
+		case North:
+			return [][]int{{x - 1, y}}, [][]int{{x + 1, y}}
+		case East:
+			return [][]int{{x - 1, y}, {x, y - 1}}, nil
+		}
+	}
+	if in == East {
+		switch out {
+		case North:
+			return nil, [][]int{{x + 1, y}, {x, y + 1}}
+		case East:
+			return [][]int{{x, y - 1}}, [][]int{{x, y + 1}}
+		case South:
+			return [][]int{{x + 1, y}, {x, y - 1}}, nil
+		}
+	}
+	return nil, nil
+}
+
+func countArea(grid [][]bool) int {
+	total := 0
+	for _, y := range grid {
+		for _, x := range y {
+			if x {
+				total += 1
+			}
+		}
+	}
+	return total
+}
+
 func Run(path string) (string, string) {
 	lines := utils.LoadAsStrings(path)
-	a := TraversePipe(lines, 62, 90)
-	return strconv.Itoa(a), "B"
+	a, b := TraversePipe(lines, 62, 90)
+	return strconv.Itoa(a), strconv.Itoa(b)
 }
--- solutions/d10pipemaze/pipemaze_test.go
+++ solutions/d10pipemaze/pipemaze_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTraversePipe(t *testing.T) {
+func TestTraversePipeA(t *testing.T) {
 	tests := []struct {
 		name     string
 		input    []string
@@ -42,7 +42,91 @@ func TestTraversePipe(t *testing.T) {
 	for _, test := range tests {
 		tt := test
 		t.Run(string(tt.name), func(t *testing.T) {
-			a := TraversePipe(tt.input, tt.x, tt.y)
+			a, _ := TraversePipe(tt.input, tt.x, tt.y)
+			assert.Equal(t, tt.expected, a)
+		})
+	}
+}
+
+func TestTraversePipeB(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []string
+		expected int
+		x, y     int
+	}{
+		{
+			name: "simple",
+			input: []string{
+				"...........",
+				".S-------7.",
+				".|F-----7|.",
+				".||.....||.",
+				".||.....||.",
+				".|L-7.F-J|.",
+				".|..|.|..|.",
+				".L--J.L--J.",
+				"...........",
+			},
+			expected: 4,
+			x:        1,
+			y:        1,
+		}, {
+			name: "longer",
+			input: []string{
+				"..........",
+				".S------7.",
+				".|F----7|.",
+				".||OOOO||.",
+				".||OOOO||.",
+				".|L-7F-J|.",
+				".|II||II|.",
+				".L--JL--J.",
+				"..........",
+			},
+			expected: 4,
+			x:        1,
+			y:        1,
+		}, {
+			name: "bigger",
+			input: []string{
+				"OF----7F7F7F7F-7OOOO",
+				"O|F--7||||||||FJOOOO",
+				"O||OFJ||||||||L7OOOO",
+				"FJL7L7LJLJ||LJIL-7OO",
+				"L--JOL7IIILJS7F-7L7O",
+				"OOOOF-JIIF7FJ|L7L7L7",
+				"OOOOL7IF7||L7|IL7L7|",
+				"OOOOO|FJLJ|FJ|F7|OLJ",
+				"OOOOFJL-7O||O||||OOO",
+				"OOOOL---JOLJOLJLJOOO",
+			},
+			expected: 8,
+			x:        12,
+			y:        4,
+		}, {
+			name: "biggerstill",
+			input: []string{
+				"FF7FSF7F7F7F7F7F---7",
+				"L|LJ||||||||||||F--J",
+				"FL-7LJLJ||||||LJL-77",
+				"F--JF--7||LJLJ7F7FJ-",
+				"L---JF-JLJ.||-FJLJJ7",
+				"|F|F-JF---7F7-L7L|7|",
+				"|FFJF7L7F-JF7|JL---7",
+				"7-L-JL7||F7|L7F-7F7|",
+				"L.L7LFJ|||||FJL7||LJ",
+				"L7JLJL-JLJLJL--JLJ.L",
+			},
+			expected: 10,
+			x:        4,
+			y:        0,
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(string(tt.name), func(t *testing.T) {
+			_, a := TraversePipe(tt.input, tt.x, tt.y)
 			assert.Equal(t, tt.expected, a)
 		})
 	}
